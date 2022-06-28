d0f2bf44a5c8c21cf9103229f65712def4ba45eb
Adriana Arroyo <adriana.arroyo@ucrso.info>
2022-06-27 18:30:30 -0600

    fix: Views of Customer

--- Lab6-AdrianaAH-JonathanRM/Areas/Customer/Controllers/HomeController.cs
+++ Lab6-AdrianaAH-JonathanRM/Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace Lab6_AdrianaAH_JonathanRM.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
